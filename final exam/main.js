bdf3bfba7ae01eccf261b29d00cc92c48347bdd4
hvchunghp <89526024+hvchunghp@users.noreply.github.com>
2022-07-12 11:30:32 +0700

    upload

--- final exam/main.js
+++ final exam/main.js
@@ -1,8 +1,9 @@
 function submit() {
     let x = document.getElementById("cardNum").value;
+    let y = document.getElementById("CVV").value;
     let text;
     //   Có 2 loại thẻ: 12 số và 19 số
-    if (isNaN(x) || x < 100000000000 || x > 9999999999999999999) {
+    if (isNaN(x) || x < 100000000000 || x > 9999999999999999999 || isNaN(y) || y < 100 || y > 999) {
         text = "Input not valid";
     } else {
         text = "Input OK";
